c2eb53960a683fab7c1bc9145cc45d4b17c8ee46
최재용 <jayong93@gmail.com>
2020-03-30 14:45:24 +0900

    SharedPtr Skiplist 구현

--- 숙제1/SharedPtrLF/SharedPtrLF/LazySKIPLIST.cpp
+++ 숙제1/SharedPtrLF/SharedPtrLF/LazySKIPLIST.cpp
@@ -11,7 +11,7 @@
 using namespace std;
 using namespace chrono;
 
-static const int NUM_TEST = 4000000;
+static const int NUM_TEST = 400000;
 static const int RANGE = 1000;
 static const int MAX_LEVEL = 10;
 
@@ -20,7 +20,7 @@ class LZSKNode
 public:
 	recursive_mutex m_lock;
 	int key;
-	LZSKNode* next[MAX_LEVEL];
+	shared_ptr<LZSKNode> next[MAX_LEVEL];
 	int topLevel;
 	volatile bool marked = false;
 	volatile bool fullyLinked = false;
@@ -77,12 +77,12 @@ class LZSKSET
 {
 public:
 
-	LZSKNode* head;
-	LZSKNode* tail;
+	shared_ptr<LZSKNode> head;
+	shared_ptr<LZSKNode> tail;
 
 	LZSKSET() {
-		head = new LZSKNode(0x80000000);
-		tail = new LZSKNode(0x7FFFFFFF);
+		head = make_shared<LZSKNode>(0x80000000);
+		tail = make_shared<LZSKNode>(0x7FFFFFFF);
 		for (int i = 0; i < MAX_LEVEL; i++) {
 			head->next[i] = tail;
 		}
@@ -90,28 +90,26 @@ public:
 
 	void Init()
 	{
-		LZSKNode* curr = head->next[0];
+		shared_ptr<LZSKNode> curr = head->next[0];
 		while (curr != tail) {
-			LZSKNode* temp = curr;
 			curr = curr->next[0];
-			delete temp;
 		}
 		for (int i = 0; i < MAX_LEVEL; i++) {
 			head->next[i] = tail;
 		}
 	}
 
-	int Find(int x, LZSKNode* preds[], LZSKNode* succs[])
+	int Find(int x, shared_ptr<LZSKNode> preds[], shared_ptr<LZSKNode> succs[])
 	{
 		int bottomLevel = 0;
 		int lFound = -1;
 		bool marked = false;
-		LZSKNode* pred = head;
+		shared_ptr<LZSKNode> pred = head;
 		for (int level = MAX_LEVEL - 1; level >= bottomLevel; --level) {
-			LZSKNode* curr = pred->next[level];
+			shared_ptr<LZSKNode> curr = atomic_load(&(pred->next[level]));
 			while (x > curr->key) {
 				pred = curr;
-				curr = pred->next[level];
+				curr = atomic_load(&(pred->next[level]));
 			}
 			if (lFound == -1 && x == curr->key) {
 				lFound = level;
@@ -132,14 +130,14 @@ public:
 		}
 
 		int bottomLevel = 0;
-		LZSKNode* preds[MAX_LEVEL];
-		LZSKNode* succs[MAX_LEVEL];
+		shared_ptr<LZSKNode> preds[MAX_LEVEL];
+		shared_ptr<LZSKNode> succs[MAX_LEVEL];
 
 		while (true)
 		{
 			int lFound = Find(x, preds, succs);
 			if (lFound != -1) {
-				LZSKNode* nodeFound = succs[lFound];
+				shared_ptr<LZSKNode> nodeFound = succs[lFound];
 				if (!nodeFound->marked) {
 					while(!nodeFound->fullyLinked){}
 					return false;
@@ -148,15 +146,15 @@ public:
 			}
 			int highestLocked = -1;
 			{
-				LZSKNode* pred;
-				LZSKNode* succ;
+				shared_ptr<LZSKNode> pred;
+				shared_ptr<LZSKNode> succ;
 				bool valid = true;
 				for (int level = 0; valid && (level <= topLevel); ++level) {
 					pred = preds[level];
 					succ = succs[level];
 					pred->lock();
 					highestLocked = level;
-					valid = !pred->marked && !succ->marked && pred->next[level] == succ;
+					valid = !pred->marked && !succ->marked && atomic_load(&(pred->next[level])) == succ;
 				}
 				if (!valid) {
 					for (int level = 0; level <= highestLocked; ++level) {
@@ -164,12 +162,12 @@ public:
 					}
 					continue;
 				}
-				LZSKNode* newNode = new LZSKNode{ x, topLevel };
+				shared_ptr<LZSKNode> newNode = make_shared<LZSKNode>( x, topLevel);
 				for (int level = 0; level <= topLevel; ++level) {
 					newNode->next[level] = succs[level];
 				}
 				for (int level = 0; level <= topLevel; ++level) {
-					preds[level]->next[level] = newNode;
+					atomic_store(&(preds[level]->next[level]), newNode);
 				}
 				newNode->fullyLinked = true;
 				for (int level = 0; level <= highestLocked; ++level) {
@@ -184,9 +182,9 @@ public:
 	bool Remove(int x)
 	{
 		int bottomLevel = 0;
-		LZSKNode* preds[MAX_LEVEL];
-		LZSKNode* succs[MAX_LEVEL];
-		LZSKNode* victim = nullptr;
+		shared_ptr<LZSKNode> preds[MAX_LEVEL];
+		shared_ptr<LZSKNode> succs[MAX_LEVEL];
+		shared_ptr<LZSKNode> victim = nullptr;
 		bool isMarked = false;
 		int topLevel = -1;
 		
@@ -208,14 +206,14 @@ public:
 				}
 				int highestLocked = -1;
 				{
-					LZSKNode* pred;
-					LZSKNode* succ;
+					shared_ptr<LZSKNode> pred;
+					shared_ptr<LZSKNode> succ;
 					bool valid = true;
 					for (int level = 0; valid && (level <= topLevel); ++level) {
-						pred = preds[level];
+						pred = atomic_load(&(preds[level]));
 						pred->lock();
 						highestLocked = level;
-						valid = !pred->marked && pred->next[level] == victim;
+						valid = !pred->marked && atomic_load(&(pred->next[level])) == victim;
 					}
 					if (!valid) {
 						for (int level = 0; level <= highestLocked; ++level) {
@@ -224,7 +222,7 @@ public:
 						continue;
 					}
 					for (int level = topLevel; level >= 0; --level) {
-						preds[level]->next[level] = victim->next[level];
+						atomic_store(&(preds[level]->next[level]), victim->next[level]);
 					}
 					victim->unlock();
 					for (int level = 0; level <= highestLocked; ++level) {
@@ -242,14 +240,14 @@ public:
 	bool Contains(int x)
 	{
 		int bottomLevel = 0;
-		LZSKNode* preds[MAX_LEVEL];
-		LZSKNode* succs[MAX_LEVEL];
+		shared_ptr<LZSKNode> preds[MAX_LEVEL];
+		shared_ptr<LZSKNode> succs[MAX_LEVEL];
 		int lFound = Find(x, preds, succs);
 		return (lFound != -1 && succs[lFound]->fullyLinked && !succs[lFound]->marked);
 	}
 	void Dump()
 	{
-		LZSKNode* curr = head;
+		shared_ptr<LZSKNode> curr = head;
 		printf("First 20 entries are : ");
 		for (int i = 0; i < 20; ++i) {
 			curr = curr->next[0];
@@ -292,5 +290,4 @@ int main()
 		cout << num_thread << " Threads,  Time = ";
 		cout << duration_cast<milliseconds>(du).count() << " ms\n";
 	}
-	system("pause");
 }
