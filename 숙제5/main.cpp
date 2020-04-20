8768775040e761581323db171294f79d2bf3d7ec
SyuJyo <jayong93@gmail.com>
2020-04-21 02:51:01 +0900

    CX 버전 완성

--- 숙제5/main.cpp
+++ 숙제5/main.cpp
@@ -5,94 +5,115 @@
 #include <thread>
 #include <chrono>
 #include <mutex>
+#include <shared_mutex>
+#include <optional>
 
 using namespace std;
 using namespace std::chrono;
 
 constexpr int MAXHEIGHT = 10;
-class SLNODE {
+class SLNODE
+{
 public:
 	int key;
-	SLNODE* next[MAXHEIGHT];
+	SLNODE *next[MAXHEIGHT];
 	int height;
 	SLNODE(int x, int h)
 	{
 		key = x;
 		height = h;
-		for (auto& p : next) p = nullptr;
+		for (auto &p : next)
+			p = nullptr;
 	}
 	SLNODE(int x)
 	{
 		key = x;
 		height = MAXHEIGHT;
-		for (auto& p : next) p = nullptr;
+		for (auto &p : next)
+			p = nullptr;
 	}
 	SLNODE()
 	{
 		key = 0;
 		height = MAXHEIGHT;
-		for (auto& p : next) p = nullptr;
+		for (auto &p : next)
+			p = nullptr;
 	}
 };
 
-class SKLIST {
+class SKLIST
+{
 	SLNODE head, tail;
+
 public:
 	SKLIST()
 	{
 		head.key = 0x80000000;
 		tail.key = 0x7FFFFFFF;
 		head.height = tail.height = MAXHEIGHT;
-		for (auto& p : head.next) p = &tail;
+		for (auto &p : head.next)
+			p = &tail;
 	}
-	~SKLIST() {
+	~SKLIST()
+	{
 		Init();
 	}
 
 	void Init()
 	{
-		SLNODE* ptr;
-		while (head.next[0] != &tail) {
+		SLNODE *ptr;
+		while (head.next[0] != &tail)
+		{
 			ptr = head.next[0];
 			head.next[0] = head.next[0]->next[0];
 			delete ptr;
 		}
-		for (auto& p : head.next) p = &tail;
+		for (auto &p : head.next)
+			p = &tail;
 	}
-	void Find(int key, SLNODE* preds[MAXHEIGHT], SLNODE* currs[MAXHEIGHT])
+	void Find(int key, SLNODE *preds[MAXHEIGHT], SLNODE *currs[MAXHEIGHT])
 	{
 		int cl = MAXHEIGHT - 1;
-		while (true) {
+		while (true)
+		{
 			if (MAXHEIGHT - 1 == cl)
 				preds[cl] = &head;
-			else preds[cl] = preds[cl + 1];
+			else
+				preds[cl] = preds[cl + 1];
 			currs[cl] = preds[cl]->next[cl];
-			while (currs[cl]->key < key) {
+			while (currs[cl]->key < key)
+			{
 				preds[cl] = currs[cl];
 				currs[cl] = currs[cl]->next[cl];
 			}
-			if (0 == cl) return;
+			if (0 == cl)
+				return;
 			cl--;
 		}
 	}
 
 	bool Add(int key)
 	{
-		SLNODE* preds[MAXHEIGHT], * currs[MAXHEIGHT];
+		SLNODE *preds[MAXHEIGHT], *currs[MAXHEIGHT];
 
 		Find(key, preds, currs);
 
-		if (key == currs[0]->key) {
+		if (key == currs[0]->key)
+		{
 			return false;
 		}
-		else {
+		else
+		{
 			int height = 1;
-			while (rand() % 2 == 0) {
+			while (rand() % 2 == 0)
+			{
 				height++;
-				if (MAXHEIGHT == height) break;
+				if (MAXHEIGHT == height)
+					break;
 			}
-			SLNODE* node = new SLNODE(key, height);
-			for (int i = 0; i < height; ++i) {
+			SLNODE *node = new SLNODE(key, height);
+			for (int i = 0; i < height; ++i)
+			{
 				preds[i]->next[i] = node;
 				node->next[i] = currs[i];
 			}
@@ -102,29 +123,34 @@ public:
 	}
 	bool Remove(int key)
 	{
-		SLNODE* preds[MAXHEIGHT], * currs[MAXHEIGHT];
+		SLNODE *preds[MAXHEIGHT], *currs[MAXHEIGHT];
 
 		Find(key, preds, currs);
 
-		if (key == currs[0]->key) {
-			for (int i = 0; i < currs[0]->height; ++i) {
+		if (key == currs[0]->key)
+		{
+			for (int i = 0; i < currs[0]->height; ++i)
+			{
 				preds[i]->next[i] = currs[i]->next[i];
 			}
 			delete currs[0];
 			return true;
 		}
-		else {
+		else
+		{
 			return false;
 		}
 	}
 	bool Contains(int key)
 	{
-		SLNODE* preds[MAXHEIGHT], * currs[MAXHEIGHT];
+		SLNODE *preds[MAXHEIGHT], *currs[MAXHEIGHT];
 		Find(key, preds, currs);
-		if (key == currs[0]->key) {
+		if (key == currs[0]->key)
+		{
 			return true;
 		}
-		else {
+		else
+		{
 			return false;
 		}
 	}
@@ -132,62 +158,60 @@ public:
 	void display20()
 	{
 		int c = 20;
-		SLNODE* p = head.next[0];
+		SLNODE *p = head.next[0];
 		while (p != &tail)
 		{
 			cout << p->key << ", ";
 			p = p->next[0];
 			c--;
-			if (c == 0) break;
+			if (c == 0)
+				break;
 		}
 		cout << endl;
 	}
 };
 
-enum class Func {
+enum class Func
+{
 	None,
 	Add,
 	Remove,
 	Contains,
 };
 
-struct Invoc {
+struct Invoc
+{
 	Func func;
 	int arg;
 
-	Invoc(Func func, int arg = 0) : func{ func }, arg{ arg } {}
-	Invoc(Invoc&& other) noexcept : func{ other.func }, arg{ other.arg } {}
+	Invoc(Func func, int arg = 0) : func{func}, arg{arg} {}
+	Invoc(Invoc &&other) noexcept : func{other.func}, arg{other.arg} {}
 
-	bool is_read_only() const {
+	bool is_read_only() const
+	{
 		return func == Func::Contains;
 	}
 };
 
 using Response = int;
 
-struct Node {
+struct Node
+{
 	Invoc invoc;
-	atomic<Node*> decide_next;
-	unsigned int seq;
-	atomic<Node*> next;
+	uint64_t seq;
+	atomic<Node *> next;
 
-	Node(Invoc&& invoc) : invoc{ move(invoc) }, decide_next{ nullptr }, seq{ 0 }, next{ nullptr } {}
-	static Node* max(const vector<Node*>& nodes) {
-		Node* m = nodes[0];
-		for (auto it = nodes.begin() + 1; it != nodes.end(); ++it) {
-			if (m->seq < (*it)->seq) {
-				m = *it;
-			}
-		}
-		return m;
-	}
+	Node(Invoc &&invoc) : invoc{move(invoc)}, seq{0}, next{nullptr} {}
 };
 
-struct Object {
+struct Object
+{
 	SKLIST container;
 
-	Response apply(const Invoc& invoc) {
-		switch (invoc.func) {
+	Response apply(const Invoc &invoc)
+	{
+		switch (invoc.func)
+		{
 		case Func::Add:
 			return container.Add(invoc.arg);
 		case Func::Remove:
@@ -200,23 +224,36 @@ struct Object {
 	}
 };
 
+struct Combined
+{
+	Object obj;
+	shared_mutex rw_lock;
+	Node *last_node;
+
+	Combined(Node &node) : last_node{&node} {}
+};
+
 constexpr int RECYCLE_RATE = 100;
 
-class OLFUniversal {
+class OLFUniversal
+{
 public:
-	OLFUniversal(int capacity) : capacity{ capacity }, invoke_num{ 0 } {
-		Invoc invoc{ Func::None };
+	OLFUniversal(int capacity) : capacity{capacity}, invoke_num{0}
+	{
+		Invoc invoc{Func::None};
 		tail = new Node(move(invoc));
 		tail->seq = 1;
-		head.resize(capacity, tail);
-		last_nodes.resize(capacity, tail);
-		objects.resize(capacity);
+		head.store(tail, memory_order_relaxed);
+		for(auto i=0;i<capacity;++i) {
+		combined_list.emplace_back(new Combined(*tail));
+		}
 	}
 	~OLFUniversal()
 	{
-		Node* cur = tail->next.load(memory_order_relaxed);
-		while (cur->next != nullptr) {
-			Node* del = cur;
+		Node *cur = tail->next.load(memory_order_relaxed);
+		while (cur->next != nullptr)
+		{
+			Node *del = cur;
 			cur = cur->next.load(memory_order_relaxed);
 			delete del;
 		}
@@ -224,47 +261,60 @@ public:
 		delete tail;
 	}
 
-	Object& current_obj() {
-		Node* m = head[0];
+	Object &current_obj()
+	{
+		Node *m = combined_list[0]->last_node;
 		int thread_id = 0;
 		int index = 1;
-		for (auto it = head.begin() + 1; it != head.end(); ++it, ++index) {
-			if (m->seq < (*it)->seq) {
-				m = *it;
+		for (auto it = combined_list.begin() + 1; it != combined_list.end(); ++it, ++index)
+		{
+			if (m->seq < (*it)->last_node->seq)
+			{
+				m = (*it)->last_node;
 				thread_id = index;
 			}
 		}
-		return objects[thread_id];
+		return combined_list[thread_id]->obj;
 	}
 
-	Response apply(Invoc&& invoc, int thread_id) {
-		if (invoc.is_read_only()) {
-			return do_read_only(move(invoc), thread_id);
-		}
+	template <typename LOCK, typename F>
+	optional<pair<LOCK, Combined &>> get_max_comb(
+		bool try_until_success, F cond)
+	{
+		LOCK lg;
+		do
+		{
+			for (auto comb : combined_list)
+			{
+				lg = LOCK{comb->rw_lock, try_to_lock};
+				if (lg && cond(*comb))
+				{
+					return make_pair(move(lg), ref(*comb));
+				}
+			}
+		} while (try_until_success);
+		return nullopt;
+	}
 
-		Node* prefer = new Node(move(invoc));
-		while (prefer->seq == 0) {
-			Node* before = Node::max(head);
-			Node* after = nullptr;
-			if (before->decide_next.compare_exchange_strong(after, prefer))
-				after = prefer;
-			before->next.store(after, memory_order_relaxed);
-			after->seq = before->seq + 1;
-			head[thread_id] = after;
-		}
+	Response update_local_obj(const Node &prefer, int thread_id)
+	{
+		auto ret = get_max_comb<unique_lock<shared_mutex>>(true, [](auto &_) { return true; });
+		auto &[lg, comb] = *ret;
 
-		auto& last_node = last_nodes[thread_id];
-		auto& last_obj = objects[thread_id];
+		auto &last_node = comb.last_node;
+		auto &last_obj = comb.obj;
 
 		last_node = last_node->next.load(memory_order_relaxed);
-		while (last_node != prefer)
+		while (last_node != &prefer)
 		{
 			last_obj.apply(last_node->invoc);
 			last_node = last_node->next.load(memory_order_relaxed);
 		}
 
-		if (invoke_num.load(memory_order_relaxed) < RECYCLE_RATE) {
-			if (invoke_num.fetch_add(1, memory_order_relaxed) + 1 == RECYCLE_RATE) {
+		if (invoke_num.load(memory_order_relaxed) < RECYCLE_RATE)
+		{
+			if (invoke_num.fetch_add(1, memory_order_relaxed) + 1 == RECYCLE_RATE)
+			{
 				recycle();
 				invoke_num.store(0, memory_order_relaxed);
 			}
@@ -273,90 +323,121 @@ public:
 		return last_obj.apply(last_node->invoc);
 	}
 
-	Response do_read_only(Invoc&& invoc, int thread_id) {
-		auto old_head = Node::max(head);
-		auto& last_node = last_nodes[thread_id];
-		auto& last_obj = objects[thread_id];
-
-		if (last_node == old_head) {
-			return last_obj.apply(invoc);
+	Response apply(Invoc &&invoc, int thread_id)
+	{
+		if (invoc.is_read_only())
+		{
+			optional<Response> ret_val = do_read_only(move(invoc), thread_id);
+			if (ret_val)
+			{
+				return *ret_val;
+			}
 		}
 
-		last_node = last_node->next.load(memory_order_relaxed);
-
-		while (last_node != old_head) {
-			last_obj.apply(last_node->invoc);
-			last_node = last_node->next.load(memory_order_relaxed);
+		Node *old_head = head.load(memory_order_relaxed);
+		Node *prefer = new Node(move(invoc));
+		while (true)
+		{
+			prefer->seq = old_head->seq + 1;
+			if (head.compare_exchange_strong(old_head, prefer))
+				break;
 		}
-		last_obj.apply(last_node->invoc);
 
-		return last_obj.apply(invoc);
+		return update_local_obj(*prefer, thread_id);
 	}
 
-	void recycle() {
-		Node* min = last_nodes[0];
-		for (auto i = 1; i < last_nodes.size(); ++i) {
-			if (last_nodes[i]->seq < min->seq) {
-				min = last_nodes[i];
-			}
+	optional<Response> do_read_only(Invoc &&invoc, int thread_id)
+	{
+		auto old_head = head.load(memory_order_relaxed);
+		auto old_seq = old_head->seq;
+
+		auto ret = get_max_comb<shared_lock<shared_mutex>>(false, [old_seq](const Combined &c) { return c.last_node->seq == old_seq; });
+		if (ret)
+		{
+			auto& [lg, comb] = *ret;
+			return comb.obj.apply(invoc);
 		}
-		for (auto i = 0; i < head.size(); ++i) {
-			if (head[i]->seq < min->seq) {
-				min = head[i];
+		return nullopt;
+	}
+
+	void recycle()
+	{
+		Node *min = combined_list[0]->last_node;
+		for (auto i = 1; i < combined_list.size(); ++i)
+		{
+			auto &combined = combined_list[i];
+			if (combined->last_node->seq < min->seq)
+			{
+				min = combined->last_node;
 			}
 		}
 
-		if (min == tail) { return; }
+		if (min == tail)
+		{
+			return;
+		}
 
 		auto old_next = tail->next.load(memory_order_relaxed);
 		tail->next.store(min, memory_order_relaxed);
-		while (old_next != min) {
+		while (old_next != min)
+		{
 			auto tmp = old_next;
 			old_next = old_next->next.load(memory_order_relaxed);
 			delete tmp;
 		}
 	}
+
 private:
-	vector<Node*> head;
-	vector<Node*> last_nodes;
-	vector<Object> objects;
-	Node* tail;
+	// 가장 최근 Node
+	atomic<Node *> head;
+	vector<Combined*> combined_list;
+	// 가장 오래된 Node
+	Node *tail;
 	int capacity;
 	atomic_ullong invoke_num;
 };
 
 const auto NUM_TEST = 4000000;
 const auto KEY_RANGE = 1000;
-void ThreadFunc(OLFUniversal* list, int num_thread, int thread_id)
+void ThreadFunc(OLFUniversal *list, int num_thread, int thread_id)
 {
 	int key;
 
-	for (int i = 0; i < NUM_TEST / num_thread; i++) {
-		switch (rand() % 3) {
-		case 0: key = rand() % KEY_RANGE;
+	for (int i = 0; i < NUM_TEST / num_thread; i++)
+	{
+		switch (rand() % 3)
+		{
+		case 0:
+			key = rand() % KEY_RANGE;
 			list->apply(Invoc(Func::Add, key), thread_id);
 			break;
-		case 1: key = rand() % KEY_RANGE;
+		case 1:
+			key = rand() % KEY_RANGE;
 			list->apply(Invoc(Func::Remove, key), thread_id);
 			break;
-		case 2: key = rand() % KEY_RANGE;
+		case 2:
+			key = rand() % KEY_RANGE;
 			list->apply(Invoc(Func::Contains, key), thread_id);
 			break;
-		default: cout << "Error\n";
+		default:
+			cout << "Error\n";
 			exit(-1);
 		}
 	}
 }
 
-int main() {
-	for (auto n = 1; n <= 64; n *= 2) {
+int main()
+{
+	for (auto n = 1; n <= 64; n *= 2)
+	{
 		OLFUniversal list(n);
 
-		vector <thread> threads;
+		vector<thread> threads;
 		auto s = high_resolution_clock::now();
 		for (int i = 0; i < n; ++i)
 			threads.emplace_back(ThreadFunc, &list, n, i);
-		for (auto& th : threads) th.join();
+		for (auto &th : threads)
+			th.join();
 		auto d = high_resolution_clock::now() - s;
 
 		list.current_obj().container.display20();
